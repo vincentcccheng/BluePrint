1348d7216acf1c7d645c666b707a38d1b4092b5a
vincentcccheng <vincent-cheng@live.com>
2020-07-01 15:44:29 +0800

    added v1

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect,  url_for
+from minnows import minnows
+from fish import fish
+from sharks import sharks
+
+#app = Flask(__name__)
+app = Flask('blueprints_example2')
+app.register_blueprint(minnows, url_prefix='/minnows')
+app.register_blueprint(fish, url_prefix='/fish')
+app.register_blueprint(sharks, url_prefix='/sharks')
+
+@app.route("/")
+def index():
+   return redirect('/sharks')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True)
